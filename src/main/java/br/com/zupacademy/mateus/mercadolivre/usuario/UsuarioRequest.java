7dfcc2484cd4708998380263c89f7cd578b32ae3
Mateus-A-Soares <soares.almeida.mateus@gmail.com>
2021-04-10 16:51:18 -0300

    Desenvolvida classe request do Usuario

---
+++ src/main/java/br/com/zupacademy/mateus/mercadolivre/usuario/UsuarioRequest.java
@@ -0,0 +1,51 @@
+package br.com.zupacademy.mateus.mercadolivre.usuario;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+/**
+ * 
+ * Classe modelo que representa os dados nas requests de cadastro de usuários
+ * 
+ * @author Mateus Soares
+ */
+public class UsuarioRequest {
+	
+	@NotBlank
+	@Email
+	private String login;
+	
+	@NotBlank
+	@Size(max = 6)
+	private String senha;
+
+	/**
+	 * Construtor que instância um objeto UsuarioRequest com os dados representativos da entidade Usuario.
+	 * 
+	 * @param login login do usuário, deve estar formatada como email e não pode estar vazia.
+	 * @param senha senha do usuário, não pode estar nula e tem tamanho mínimo de seis caracteres.
+	 */
+	public UsuarioRequest(@NotBlank @Email String login, @NotBlank @Size(min = 6) String senha) {
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+	
+	/**
+	 * Transforma o objeto UsuarioRequest em um objeto Usuario.
+	 * 
+	 * @return objeto Usuario populado com os dados desse objeto.
+	 */
+	public Usuario toModel() {
+		return new Usuario(login, senha);
+	}
+}
