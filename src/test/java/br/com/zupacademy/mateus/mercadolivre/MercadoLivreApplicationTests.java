9799693375da090ece46cfcff453f59e677646d5
Mateus-A-Soares <soares.almeida.mateus@gmail.com>
2021-04-10 12:37:18 -0300

    Configuração inicial do projeto

---
+++ src/test/java/br/com/zupacademy/mateus/mercadolivre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.mateus.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
