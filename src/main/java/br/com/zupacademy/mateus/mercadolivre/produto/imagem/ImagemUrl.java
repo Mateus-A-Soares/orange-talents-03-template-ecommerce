a5a9356df72ff8481bbac16e636319046a280d24
Mateus-A-Soares <soares.almeida.mateus@gmail.com>
2021-04-14 11:38:53 -0300

    Classe ImagemUrl modelada

---
+++ src/main/java/br/com/zupacademy/mateus/mercadolivre/produto/imagem/ImagemUrl.java
@@ -0,0 +1,57 @@
+package br.com.zupacademy.mateus.mercadolivre.produto.imagem;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.URL;
+
+import br.com.zupacademy.mateus.mercadolivre.produto.Produto;
+
+/**
+ * 
+ * Classe modelo para geração da tabela que interliga o path das imagens a um produto.
+ * 
+ * @author Mateus Soares
+ */
+public class ImagemUrl {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@ManyToOne(optional = false)
+	private Produto produto;
+	
+	@URL @NotBlank
+	@Column(nullable = false, unique = true)
+	private String url;
+
+	/**
+	 * Construtor que instancia um objeto {@link ImagemUrl}
+	 * 
+	 * @param produto	produto ao qual a imagem pertence, obrigatório;
+	 * @param url		url de acesso a imagem, deve estar formatado como url, ser único e é obrigatório;
+	 */
+	public ImagemUrl(@NotNull Produto produto, @URL @NotBlank String url) {
+		this.produto = produto;
+		this.url = url;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+}
