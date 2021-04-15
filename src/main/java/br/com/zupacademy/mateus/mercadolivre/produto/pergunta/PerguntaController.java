744c0496ce0c6894532a606296a38a94ce10d173
Mateus-A-Soares <soares.almeida.mateus@gmail.com>
2021-04-15 10:48:58 -0300

    End point de cadastro de perguntas desenvolvido

---
+++ src/main/java/br/com/zupacademy/mateus/mercadolivre/produto/pergunta/PerguntaController.java
@@ -0,0 +1,55 @@
+package br.com.zupacademy.mateus.mercadolivre.produto.pergunta;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.com.zupacademy.mateus.mercadolivre.auth.UserCredentials;
+import br.com.zupacademy.mateus.mercadolivre.produto.Produto;
+import br.com.zupacademy.mateus.mercadolivre.produto.ProdutoResponse;
+
+/**
+ * 
+ * Controller com os end-points relacionados ao CRUD das perguntas cadastradas para um produto.
+ * 
+ * @author Mateus Soares
+ */
+@RestController
+@RequestMapping("/produtos/{id}/pergunta")
+public class PerguntaController {
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	/**
+	 * End-point de URL /produtos/{id}/pergunta que realiza a validação e o
+	 * cadastro do registro das perguntas relacionadas ao produto de Id passado na URL.
+	 * 
+	 * @param produtoId		Id do produto qual pertence a pergunta a ser cadastrada;
+	 * @param request		Pergunta a ser cadastrada; 
+	 * @param credentials	Credenciais do usuário logado que será anexado a pergunta.
+	 * @return ResponseEntity representando o status HTTP 200, 400 ou 500.
+	 */
+	@PatchMapping
+	@Transactional
+	public ResponseEntity<ProdutoResponse> cadastra(@PathVariable("id") Long produtoId, @Valid @RequestBody PerguntaRequest request,
+			@AuthenticationPrincipal UserCredentials credentials) {
+		Produto produto = manager.find(Produto.class, produtoId);
+		if(produto == null)
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado");
+		Pergunta pergunta = request.toModel(manager, credentials.toModel());
+		manager.persist(pergunta);
+		return ResponseEntity.ok().build();
+	}
+}
--- src/main/java/br/com/zupacademy/mateus/mercadolivre/produto/pergunta/PerguntaRequest.java
+++ src/main/java/br/com/zupacademy/mateus/mercadolivre/produto/pergunta/PerguntaRequest.java
@@ -7,18 +7,27 @@ import br.com.zupacademy.mateus.mercadolivre.produto.Produto;
 import br.com.zupacademy.mateus.mercadolivre.shared.validation.constraints.ExistsOne;
 import br.com.zupacademy.mateus.mercadolivre.usuario.Usuario;
 
+/**
+ * 
+ * Classe modelo que representa as imagens enviadas nas requests de cadastro de perguntas para os produtos.
+ * 
+ * @author Mateus Soares
+ */
 public class PerguntaRequest {
 	
 	@NotBlank
 	private String titulo;
 
 	@NotNull
-	private Usuario usuario;
-
-	@NotNull
 	@ExistsOne(entityTargetClass = Produto.class, fieldTargetName = "id")
 	private Long produtoId;
 
+	/**
+	 * Construtor que instância um objeto {@link PerguntaRequest}
+	 * 
+	 * @param titulo	título da pergunta, obrigatório;
+	 * @param produtoId	id do produto a qual a pergunta pertence, obrigatório e existente.
+	 */
 	public PerguntaRequest(@NotBlank String titulo, @NotNull Long produtoId) {
 		this.titulo = titulo;
 		this.produtoId = produtoId;
