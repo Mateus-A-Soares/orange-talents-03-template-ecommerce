b7d7de6bcb9e56081894634db7c33c4490a3220f
Mateus-A-Soares <soares.almeida.mateus@gmail.com>
2021-04-20 10:03:19 -0300

    Implementação do serviço de email para o profile de produção

--- src/main/java/br/com/zupacademy/mateus/mercadolivre/shared/email/MailSenderProdProfile.java
+++ src/main/java/br/com/zupacademy/mateus/mercadolivre/shared/email/MailSenderProdProfile.java
@@ -1,14 +1,29 @@
 package br.com.zupacademy.mateus.mercadolivre.shared.email;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
-@Profile({"prod", "test"})
+@Profile({ "prod", "test" })
 @Component
 public class MailSenderProdProfile implements MailSender {
 
+	@Value("${spring.mail.username}")
+	private static String fromUsername;
+	
+	@Autowired
+	private JavaMailSender emailSender;
+
 	@Override
 	public void execute(String recipient, String subject, String body) {
-		System.out.println("to: " + recipient + ", subject: " + subject + ", message: " + body);
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom(fromUsername);
+		message.setTo(recipient);
+		message.setSubject(subject);
+		message.setText(body);
+		emailSender.send(message);
 	}
 }
